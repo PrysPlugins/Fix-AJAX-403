24f218412ad51174600ca056ff2086797f3c9a55
Jeremy Pry <JPry@users.noreply.github.com>
2014-05-29 09:44:48 -0400

    Create ajax_403.php

---
+++ ajax_403.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Fix AJAX 403
+ * Plugin URI: https://github.com/PrysPlugins/Fix-AJAX-403
+ * Description: Prevent bad AJAX login requests from generating a 403 code
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ * GitHub Plugin URI: https://github.com/PrysPlugins/Fix-AJAX-403
+ * GitHub Branch: master
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+add_action( 'plugins_loaded', 'jpry_adjust_wpe_hooks' );
+function jpry_adjust_wpe_hooks() {
+	remove_action( 'wp_login_failed', 'wpe_login_failed_403' );
+	add_action( 'wp_login_failed', 'jpry_login_failed_403' );
+}
+
+function jpry_login_failed_403() {
+	// Don't 403 for Ajax requests
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		return;
+	}
+	
+	status_header( 403 );
+}
